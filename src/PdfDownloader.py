721e523fd909bb44132843043bd61b5ab05de384
Valentin Oechsner <oechval@hilti.com>
2021-11-12 15:59:10 +0100

    added func get table and order code

--- src/PdfDownloader.py
+++ src/PdfDownloader.py
@@ -25,7 +25,7 @@ def get_pdfs(my_url):
     print("base",base)
     for link in html_page.find_all('a'):
         current_link = link.get('href')
-        if current_link.endswith('pdf'):
+        if current_link != None and current_link.endswith('pdf'):
             if og_url:
                 print("currentLink",current_link)
                 links.append(og_url["content"] + current_link)
@@ -34,16 +34,55 @@ def get_pdfs(my_url):
 
     for link in links:
         try: 
-            wget.download(link)
+            print(link)
+            # wget.download(link)
         except:
             print(" \n \n Unable to Download A File \n")
     print('\n')
 
+def get_order_code(my_url):
+    links = []
+    html = urlopen(my_url).read()
+    html_page = bs(html, features="lxml") 
+    og_url = html_page.find("meta",  property = "og:url")
+    base = urlparse(my_url)
+    print("base",base)
+    for finding in html_page.find_all('tr'):
+        data_order_code = finding.get('data-order-code')
+        if data_order_code != None :
+            print(data_order_code)
+
+def get_table(my_url):
+    links = []
+    html = urlopen(my_url).read()
+    html_page = bs(html, features="lxml") 
+    og_url = html_page.find("meta",  property = "og:url")
+    base = urlparse(my_url)
+    print("base",base)
+    for finding in html_page.find_all('th'):
+        headerclass = finding.get('class')
+        sortable = finding.get("sortable")
+        data = finding.get_text() # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all
+        if headerclass != None and sortable != None:
+            if headerclass[0] == "productTable__table--sticky-head__moving" and sortable == "true":
+                print(data)
+
+    for finding in html_page.find_all('td'):
+        data_unit = finding.get('data-unit')
+        data_mobile_visible_index = finding.get('data-mobile-visible-index')
+        data = finding.string # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all
+        if data_unit != None and data_mobile_visible_index!= None :
+            print(data_mobile_visible_index, data, data_unit)
+
 
 def main():
-    print("Enter Link: ")
-    my_url = input()
+    # print("Enter Link: ")
+    # my_url = input()
+    my_url = "https://www.we-online.com/katalog/de/WE-TOF"
     check_validity(my_url)
-    get_pdfs(my_url)
+    # get_pdfs(my_url)
+    # get_order_code(my_url)
+    get_table(my_url)
 
 main()
+
